7d2c0229343982eb68f9b496cf95cbda0b722f0c
luizmarcello <luizmarcello.vmo@hotmail.com>
2019-03-24 07:26:26 -0300

    alteração em index.php delete

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once("config.php");
 
 //Carrega um usuário:
 //$user = new Usuario();
-//$user->loadById(1);
+//$user->loadById(23);
 //Ao invés do echo mostrar a estrutura deste objeto, ele
 //mostra o quem tem dentro do método mágico __toString,
 //da classe Usuario.php.
@@ -20,19 +20,19 @@ require_once("config.php");
 //echo json_encode($lista);
 
 //Carrega uma lista de usuários, buscando pelo login:
-//$search = Usuario::search("oo");
+//$search = Usuario::search("om");
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha:
-//suario = new Usuario();
-//suario->login("jose","12345");
+//$usuario = new Usuario();
+//$usuario->login("jose","12345");
 //Ao invés do echo mostrar a estrutura deste objeto, ele
 //mostra o quem tem dentro do método mágico __toString,
 //da classe Usuario.php.
 //echo $usuario;
 
 //Insere o usuário do parâmetro na tabela, no próximo ID da sequência:
-//$aluno = new Usuario("peter","@123dificil@");
+//$aluno = new Usuario("LuizMarcellooo","--@123RolandiA@");
 //$aluno->insert();
 //echo $aluno;
 
@@ -40,19 +40,19 @@ require_once("config.php");
 //Altera o login e a senha do usuario referente ao ID do parâmetro, na invocação do método 'loadById()',
 //pelo login e senha que constam no parâmetro, na invocação do método 'update()':
 //$usuario = new Usuario();
-//$usuario->loadById(13);
-//$usuario->update("Issac","123OliveirA123");
+//$usuario->loadById(19);
+//$usuario->update("Variolona","123OFreeeeE123");
 //echo $usuario;
 
 
 //Deleta o registro referente ao índice do parâmetro na
 //invocação do método 'loadById()':
-//1º)Os valores do usuário das referidas colunas do ID são
+//1º)Os valores do registro do usuário do ID são
 //alimentados nas memórias dos atributos.
 //2º)O método 'delete()' deleta na tabela o usuário deste ID,
 //e depois limpa os registros do sistema com dados em branco.
 $usuario = new Usuario();
-$usuario->loadById(8);
+$usuario->loadById(19);
 $usuario->delete();
 echo $usuario;
 
